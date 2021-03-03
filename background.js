ab703fed76af6229332629b6e4cc50f375f18560
Hannah Austin <hannah.austin@ibm.com>
2021-03-03 20:30:08 +0000

    you make popup, RIGHT TO JAIL

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 // You read this code, right to jail!
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  let url = chrome.runtime.getURL("jail.mp3");
-  console.log(url);
-  let a = new Audio(url);
-  a.play();
-});
+let url = chrome.runtime.getURL("jail.mp3");
+console.log(url);
+let a = new Audio(url);
+a.play();
